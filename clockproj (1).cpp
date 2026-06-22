a8f91a7c0a06e5ae775670ba0d6633e580b23aa0
justingraham23982-lab <justingraham23982@gmail.com>
2026-06-22 18:13:18 -0500

    Update clockproj (1).cpp

--- clockproj (1).cpp
+++ clockproj (1).cpp
@@ -1,162 +1,105 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
+// Function to format and display the 12-hour clock
+void display12Hour(int hour, int minute, int second) {
+    int displayHour = hour % 12;
+    if (displayHour == 0) displayHour = 12;
 
+    string ampm = (hour < 12) ? "AM" : "PM";
 
-void displayTime(int hour, int minute, int second) {
-
-    cout << "Current Time: "
-
-         << hour << ":" << minute << ":" << second << endl;
-
+    cout << "**************************\n";
+    cout << "*     12-Hour Clock      *\n";
+    cout << "*     " 
+         << setw(2) << setfill('0') << displayHour << ":"
+         << setw(2) << setfill('0') << minute << ":"
+         << setw(2) << setfill('0') << second << " " << ampm
+         << "     *\n";
+    cout << "**************************\n";
 }
 
+// Function to format and display the 24-hour clock
+void display24Hour(int hour, int minute, int second) {
+    cout << "**************************\n";
+    cout << "*     24-Hour Clock      *\n";
+    cout << "*     "
+         << setw(2) << setfill('0') << hour << ":"
+         << setw(2) << setfill('0') << minute << ":"
+         << setw(2) << setfill('0') << second
+         << "          *\n";
+    cout << "**************************\n";
+}
 
+// Display both clocks
+void displayBothClocks(int hour, int minute, int second) {
+    display12Hour(hour, minute, second);
+    display24Hour(hour, minute, second);
+}
 
+// Add one hour
 void addHour(int &hour) {
-
     hour = (hour + 1) % 24;
-
 }
 
-
-
+// Add one minute
 void addMinute(int &minute, int &hour) {
-
     minute++;
-
     if (minute == 60) {
-
         minute = 0;
-
-        hour = (hour + 1) % 24;
-
+        addHour(hour);
     }
-
 }
 
-
-
+// Add one second
 void addSecond(int &second, int &minute, int &hour) {
-
     second++;
-
     if (second == 60) {
-
         second = 0;
-
         addMinute(minute, hour);
-
     }
-
 }
 
-
-
-int displayMenu() {
-
-    cout << "\nMenu:\n";
-
-    cout << "1. Add Hour\n";
-
-    cout << "2. Add Minute\n";
-
-    cout << "3. Add Second\n";
-
-    cout << "4. Exit\n";
-
-    cout << "Choose an option: ";
-
-
-
-    int choice;
-
-    cin >> choice;
-
-    return choice;
-
+// Display menu
+void displayMenu() {
+    cout << "\n**************************\n";
+    cout << "* 1 - Add One Hour       *\n";
+    cout << "* 2 - Add One Minute     *\n";
+    cout << "* 3 - Add One Second     *\n";
+    cout << "* 4 - Exit Program       *\n";
+    cout << "**************************\n";
+    cout << "Enter your choice: ";
 }
-
-
 
 int main() {
-
     int hour = 0;
-
     int minute = 0;
-
     int second = 0;
-
-
-
-    cout << "Enter starting hour (0-23): ";
-
-    cin >> hour;
-
-
-
-    cout << "Enter starting minute (0-59): ";
-
-    cin >> minute;
-
-
-
-    cout << "Enter starting second (0-59): ";
-
-    cin >> second;
-
-
 
     int choice = 0;
 
-
-
     while (choice != 4) {
-
-        displayTime(hour, minute, second);
-
-        choice = displayMenu();
-
-
+        displayBothClocks(hour, minute, second);
+        displayMenu();
+        cin >> choice;
 
         switch (choice) {
-
             case 1:
-
                 addHour(hour);
-
                 break;
-
             case 2:
-
                 addMinute(minute, hour);
-
                 break;
-
             case 3:
-
                 addSecond(second, minute, hour);
-
                 break;
-
             case 4:
-
                 cout << "Exiting program...\n";
-
                 break;
-
             default:
-
-                cout << "Invalid choice.\n";
-
+                cout << "Invalid choice. Try again.\n";
         }
-
     }
 
-
-
     return 0;
-
 }
-
